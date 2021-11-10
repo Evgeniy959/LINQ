ce6b8341c9ebffaea1b2ea53193ce5b660ec81d8
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2021-11-10 11:53:10 +0700

    Add project files.

---
+++ LINQ/Messages.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    static class Messages
+    {
+        public static void Info()
+        {
+            Console.WriteLine("Выберите пункт меню:");
+            Console.WriteLine("1 - Отобразить всю информацию о странах");
+            Console.WriteLine("2 - Отобразить название стран");
+            Console.WriteLine("3 - Отобразить название столиц");
+            Console.WriteLine("4 - Отобразить название всех европейских стран");
+            Console.WriteLine("5 - Отобразить название стран с площадью большей конкретного числа");
+        }
+        public static void Error()
+        {
+            Console.WriteLine("Ошибка! Введен неправильный пункт меню.");
+        }
+        public static void ShowResult(IEnumerable<object> items)
+        {
+            foreach (var item in items)
+            {
+                if (item is Country t)
+                    Console.WriteLine($"Страна: {t.name}\nСтолица: {t.capital}\nНаселение: {t.population} человек\nПлощадь: {t.territory} км^2\nЧасть света: {t.partOfTheWorld}\n");
+                else Console.WriteLine($"{item}");
+            }
+
+        }
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        delegate void Operation(IEnumerable<object> Obj);
+        static void Main()
+        {
+
+
+            Country Russia = new Country("Russia", "Moscow", 146748590, 17098246, "Europe");
+            Country Latvia = new Country("Latvia", "Riga", 1893700, 64589, "Europe");
+            Country China = new Country("China", "Beijing", 1442965000, 9596961, "Asia");
+            Country Kenya = new Country("Kenya", "Nairobi", 54985698, 580367, "Africa");
+            Country USA = new Country("USA", "Washington", 332278200, 9826675, "North America");
+
+            List<Country> countries = new List<Country>() { Russia, Latvia, China, Kenya, USA };
+
+            List<IEnumerable<object>> selects = new List<IEnumerable<object>>()
+            {
+                 //countries.Select(e => e.name),
+                countries.Select(item => item),
+                countries.Select(item => item.name),
+                countries.Select(item => item.capital),
+                countries.Where(item=>item.partOfTheWorld=="Europe").Select(item=>item.name),
+                countries.Where(item => item.territory > 1400000 ).Select(item => item.name)
+            };
+
+            //Operation Op = (items) => { Messages.ShowResult(items); };
+            Operation Op = (items) =>  Messages.ShowResult(items); 
+
+            Dictionary<string, Operation> menu = new Dictionary<string, Operation>
+            {
+                  {"1", Op },
+                  {"2", Op },
+                  {"3", Op },
+                  {"4", Op },
+                  {"5", Op }
+            };
+
+            Messages.Info();
+            var key = Console.ReadLine();
+            if (!menu.ContainsKey(key)) Messages.Error();
+            else menu[key](selects[Convert.ToInt32(key) - 1]);
+            //menu[key](selects[0]);
+        }
+    }
+}
